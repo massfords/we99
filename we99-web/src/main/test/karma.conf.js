66aa463b7079c5b3ca8c341946286acb9d28970c
tim-stefanski <tstefanski@gmail.com>
2015-03-10 22:59:46 -0400

    updated login pages and scripts

--- we99-web/src/main/test/karma.conf.js
+++ we99-web/src/main/test/karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function(config) {
       'webapp/bower_components/angular-sanitize/angular-sanitize.js',
       'webapp/bower_components/angular-touch/angular-touch.js',
       'webapp/bower_components/d3/d3.js',
+      'webapp/bower_components/angular-smart-table/dist/smart-table.js',
       'webapp/bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
