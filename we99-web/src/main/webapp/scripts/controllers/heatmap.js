f5ef329d16e005fcf2a9e76104191ac980b984ad
Sean Sinnott <ssinnott747@gmail.com>
2015-03-19 04:51:31 -0400

    - heatmap changes
    - refactored out d3 into a javascript class
    - cleaned up the heat map display a bit

--- we99-web/src/main/webapp/scripts/controllers/heatmap.js
+++ we99-web/src/main/webapp/scripts/controllers/heatmap.js
@@ -4,17 +4,17 @@
  * @ngdoc function
  * @name we99App.controller:HeatmapCtrl
  * @description
+ *
  * # HeatmapCtrl
  * Controller of the we99App
  */
 angular.module('we99App')
-  .controller('HeatmapCtrl', ["$scope", "RestService", "d3Service", function ($scope, RestService, d3Service) {
+  .controller('HeatmapCtrl', ["$scope", "RestService", function ($scope, RestService) {
+
+    var v = new DataVis();
 
     // Stores the index of the presently selected value in the display box.
     var selectedIndex = 0;
-
-    // Stores the presently selected result value.
-    var resultValueIndex = 0;
 
     // Stores the data set.
     var dataSetLink = null;
@@ -29,17 +29,16 @@ angular.module('we99App')
      *
      * @param data The data to be rendered in the display box.
      */
-    function renderDisplayBox(resultValueIndex, selectedIndex){
+    function renderDisplayBox(){
       displayBox.html("");
-      console.log(resultValueIndex, selectedIndex);
-      d3Service.renderSingleHeatMap({
+      v.renderSingleHeatMap({
         location: "#display-box",
-        data: dataSetLink[resultValueIndex][selectedIndex].data,
-        title: dataSetLink[resultValueIndex][selectedIndex].name,
+        data: dataSetLink[selectedIndex].data,
+        title: dataSetLink[selectedIndex].name,
         onCellClick: function(d) {
-          dataSetLink[resultValueIndex][selectedIndex].data[d.wellIndex].included =
-            !dataSetLink[resultValueIndex][selectedIndex].data[d.wellIndex].included;
-          renderDisplayBox(resultValueIndex, selectedIndex);
+          dataSetLink[selectedIndex].data[d.wellIndex].included =
+            !dataSetLink[selectedIndex].data[d.wellIndex].included;
+          renderDisplayBox();
         }
       });
     }
@@ -55,70 +54,78 @@ angular.module('we99App')
 
       listBox.html("");
       var renderTargets = [];
-      var listMaps = listBox.selectAll("svg")
-        .data(dataSetLink[resultValueIndex])
+
+      listBox.selectAll("svg")
+        .data(dataSetLink)
         .enter()
         .append("svg")
-        .attr("width","120")
-        .attr("height","150")
+        .attr("width","75")
+        .attr("height","75")
         .attr("id", function(d, i) {
           var id = "list-map-" + i;
           renderTargets.push(id);
           return id;
         })
+        .style("border", function(d, i) {
+          if(i=== selectedIndex){
+            return "solid black 1px";
+          }
+        })
         .on('click', function(d, i) {
           selectedIndex = i;
-          renderDisplayBox(resultValueIndex, selectedIndex);
+          renderAll(dataSetLink);
         } );
 
       for(var i = 0; i < renderTargets.length; i++){
 
-        d3Service.renderSingleHeatMap({
+        v.renderSingleHeatMap({
           location: "#" + renderTargets[i],
-          data: dataSetLink[resultValueIndex][i].data,
-          title: dataSetLink[resultValueIndex][i].name,
+          data: dataSetLink[i].data,
+          title: dataSetLink[i].name,
           mapFormat: {
-            margin: 8,
-            fixedsize_x: 90,
-            fixedsize_y: 90
+            fixedsize_x: 65,
+            fixedsize_y: 65
           }
         });
 
-      }
-
-      renderDisplayBox(resultValueIndex, selectedIndex);
-
-    }
-
-    // Default Data
-    var colName = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
-      rowName = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-      resultOptions = ['Sodium (Na)', 'Potassium (K)', 'Urea', 'Glucose (fasting)'];
-
-
-    var allDataSets = [];
-    for (var opt = 0; opt < resultOptions.length; opt++) {
-      var dataSet = [];
-      for (var num = 0; num < 15; num++) {
-        var array = [];
-        var index = 0;
-        for (var i = 0; i < colName.length; i++) {
-          for (var j = 0; j < rowName.length; j++) {
-            array.push({wellIndex: index++, row: i, col: j, value: Math.random(), included: true});
-          }
+        if(i != selectedIndex) {
+          v.addDarkOverlay(
+            {location: "#" + renderTargets[i]}
+          );
         }
-        dataSet[num] = array;
       }
-      allDataSets[opt] = dataSet;
+      renderDisplayBox(selectedIndex);
     }
+
+
+    var dataSet = [];
+    for (var num = 0; num < 15; num++) {
+      var array = [];
+      var index = 0;
+      for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+
+          array.push({
+            wellIndex: index++,
+            row: i,
+            col: j,
+            value: Math.random(),
+            included: true,
+            wellType: "NORMAL"
+          });
+
+          if(Math.random() <= 0.05){
+            array[array.length - 1].wellType = "CONTROL";
+          }
+
+        }
+      }
+      dataSet[num] = array;
+    };
 
     var sampleJsonData = {
-      resultOptions: resultOptions,
-      dataSets: allDataSets.map(function (dataSet) {
-        return  dataSet.map(function (d) {
+      dataSets: dataSet.map(function (d) {
             var result = {
-              colName: colName,
-              rowName: rowName,
               data: d,
               name: "A" + String(Math.round(Math.random() * 400)),
               z: Math.round(Math.random() * 100) / 100,
@@ -126,22 +133,42 @@ angular.module('we99App')
             };
             result.name = result.name + " (z=" + result.z + ") (z'=" + result.z_prime + ")";
           return result;
-          })
       })
     };
 
 
-    // Result options.
-    $scope.resultOptions = sampleJsonData.resultOptions.map(function(d,i){
-      return {label: d, value: i};
-    });
-    $scope.result = $scope.resultOptions[0];
 
-    // Watch for the
-    $scope.$watch('result', function(newValue, oldValue) {
-      resultValueIndex = newValue.value;
-      renderAll(sampleJsonData.dataSets);
-    });
+    //retrieve list of experiments
+    RestService.getExperiments()
+      .success(function(response){
+
+        $scope.experiments=response.values;
+        $scope.selectedExperiment = $scope.experiments[0];
+
+        $scope.$watch('selectedExperiment', function(newValue, oldValue){
+
+          console.log(newValue);
+          RestService.getExperimentPlates(newValue.id)
+            .success(function(response){
+              var arr = response.values;
+              console.log(arr);
+            }).error(function(response){
+              $scope.errorText="Could not retrieve experiments list.";
+            });
+
+          $scope.$watch('selectedExperiment', function(newValue, oldValue) {
+            renderAll(sampleJsonData.dataSets);
+          });
+
+
+        });
+
+      })
+      .error(function(response){
+        $scope.errorText="Could not retrieve experiments list.";
+      });
+
+
 
   }]
 );
