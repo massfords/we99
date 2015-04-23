e91e89adbfdc2e99a872b9355f75dc4414e8ed5c
alan orcharton <alan.orcharton@gmail.com>
2015-04-23 13:45:21 -0700

    Curve Fit Points produced from fit parameters and hill equation

--- we99-domain/src/main/java/edu/harvard/we99/domain/results/analysis/CurveFitPointsFunction.java
+++ we99-domain/src/main/java/edu/harvard/we99/domain/results/analysis/CurveFitPointsFunction.java
@@ -7,17 +7,26 @@ import edu.harvard.we99.domain.FitParameter;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author alan orcharton
+ *
+ * This function takes the Experiment Points, finds the max and min points
+ * and generate new points between the max and min that have been fitted to
+ * the Best Fit curve. These points can be used for plotting the best fit
+ * curve.
+ *
+ * The constructor takes the fit parameters and the equation to draw the curve
+ *
  */
 public class CurveFitPointsFunction implements Function<List<ExperimentPoint>, List<CurveFitPoint>> {
 
     private final List<FitParameter> fitParameters;
     private final int numPoints;
     private Function<List<Double>,List<Double>> curveFunction;
-    private static final Double DEFAULT_MIN = -10.0;
-    private static final Double DEFAULT_MAX = -5.0;
+    private static final Double DEFAULT_MIN = 1.0E-10;
+    private static final Double DEFAULT_MAX = 6.309573444801929E-5;
     private static final FitEquation fitequation = FitEquation.HILLEQUATION;
 
     public CurveFitPointsFunction (List<FitParameter> fitParameters, int numPoints,
@@ -46,18 +55,40 @@ public class CurveFitPointsFunction implements Function<List<ExperimentPoint>, L
                                     .map(ExperimentPoint::getX)
                                     .reduce(Double::max);
 
+
         Optional<Double> min = experimentPoints.stream()
                 .map(ExperimentPoint::getX)
                 .reduce(Double::min);
 
-        Double step = (max.orElse(DEFAULT_MAX) - min.orElse(DEFAULT_MIN))/ (double)numPoints ;
+        Double maxlog10 = Math.log10(max.orElse(DEFAULT_MAX));
+        Double minlog10 = Math.log10(min.orElse(DEFAULT_MIN));
+
+        Double logStep = (maxlog10 - minlog10) / (double)numPoints;
 
         //generate points
-        List<Double> xpoints = new ArrayList<>();
+        List<Double> xlogpoints = new ArrayList<>();
+        Double xlogpoint = minlog10;
+        xlogpoints.add(xlogpoint);
         for(int x=0; x < numPoints; x++){
-            Double xpoint = min.orElse(DEFAULT_MIN) + step;
-            xpoints.add(xpoint);
+            xlogpoint = xlogpoint + logStep;
+            xlogpoints.add(xlogpoint);
         }
+
+        List<Double> xpoints = xlogpoints.stream()
+                    .map(xlog -> Math.pow(10,xlog))
+                    .collect(Collectors.toList());
+
+
+       // Double step = (max.orElse(DEFAULT_MAX) - min.orElse(DEFAULT_MIN))/ (double)numPoints ;
+
+//        //generate points
+//        List<Double> xpoints = new ArrayList<>();
+//        Double xpoint = min.orElse(DEFAULT_MIN);
+//        xpoints.add(xpoint);
+//        for(int x=0; x < numPoints; x++){
+//            xpoint = xpoint + step;
+//            xpoints.add(xpoint);
+//        }
 
         //if(fitequation == FitEquation.HILLEQUATION)
         Map<String,Double> fitParameterMap = new HashMap<>();
--- we99-domain/src/test/java/edu/harvard/we99/domain/results/analysis/CurveFitPointsFunctionTest.java
+++ we99-domain/src/test/java/edu/harvard/we99/domain/results/analysis/CurveFitPointsFunctionTest.java
@@ -20,11 +20,17 @@ public class CurveFitPointsFunctionTest {
             77.78792825,81.6611511,84.73043079,87.1132559 ,88.93386264,
             90.30801213,91.33565182,92.09886304,92.66278154,93.07786695};
 
+    Double[] expectedYResponses2 =  { -0.41975189, 93.07786695};
+
 
     Double[] rawX = {1e-10, 1.584893192461111e-10, 2.511886431509582e-10, 3.9810717055349694e-10, 6.309573444801942e-10, 1e-09, 1.584893192461111e-09, 2.511886431509582e-09, 3.981071705534969e-09,
             6.309573444801943e-09, 1e-08, 1.5848931924611143e-08, 2.511886431509582e-08, 3.981071705534969e-08, 6.30957344480193e-08, 1e-07, 1.584893192461114e-07, 2.5118864315095823e-07, 3.981071705534969e-07,
             6.30957344480193e-07, 1e-06, 1.584893192461114e-06, 2.5118864315095823e-06, 3.981071705534969e-06, 6.30957344480193e-06, 1e-05, 1.584893192461114e-05, 2.5118864315095822e-05,
             3.9810717055349695e-05, 6.309573444801929e-05};
+
+    Double[] rawX2 = {1e-10, 6.309573444801929e-05};
+
+    int numPoints = 30;
 
 
     @Test
@@ -41,12 +47,12 @@ public class CurveFitPointsFunctionTest {
         params.add(slope);
         params.add(ec50);
 
-        List<ExperimentPoint> input = createExperimentPoints(rawX,expectedYResponses);
+        List<ExperimentPoint> input = createExperimentPoints(rawX2,expectedYResponses2);
 
-        CurveFitPointsFunction cfpf = new CurveFitPointsFunction(params,30, FitEquation.HILLEQUATION);
+        CurveFitPointsFunction cfpf = new CurveFitPointsFunction(params,numPoints, FitEquation.HILLEQUATION);
         List<CurveFitPoint> output = cfpf.apply(input);
 
-
+        assertEquals(numPoints+1, output.size());
     }
 
 
